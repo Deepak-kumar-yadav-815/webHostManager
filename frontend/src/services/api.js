ca2f541d2551a7e90ebb74a5c02139c5d378d7c5
Deepak kumar yadav <deepakkyadav815@gmail.com>
2026-07-22 14:40:34 +0530

    connected with backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://webhostmanager-tvh1.onrender.com',
 });
 
 // We no longer use an interceptor for localStorage 'token'.
