914f694a3afe43b9e27ae0e6cfd60ad737484390
Deepak kumar yadav <deepakkyadav815@gmail.com>
2026-07-22 14:42:56 +0530

    fixing the issue

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://webhostmanager-tvh1.onrender.com',
+  baseURL: 'https://webhostmanager-tvh1.onrender.com/api',
 });
 
 // We no longer use an interceptor for localStorage 'token'.
